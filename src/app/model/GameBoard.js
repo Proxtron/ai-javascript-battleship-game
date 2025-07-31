3b8893fb669845ae470ff9d56e23379b5f371500
Proxtron <bratan_pavel@yahoo.com>
2025-07-30 21:54:12 -0700

    Add more error cases for placeShip()

--- src/app/model/GameBoard.js
+++ src/app/model/GameBoard.js
@@ -1,59 +1,68 @@
 import { OutOfBoundsError } from "../error/Error";
 
 export default class GameBoard {
-    grid = [];
-    sideLength = 10;
-    INITAL_CELL_STATE = [null, false];
+	grid = [];
+	sideLength = 10;
+	INITAL_CELL_STATE = [null, false];
 
-    static NORTH = 0;
-    static EAST = 1;
-    static SOUTH = 2;
-    static WEST = 3;
+	static NORTH = 0;
+	static EAST = 1;
+	static SOUTH = 2;
+	static WEST = 3;
 
-    constructor() {
-        this.#initalizeGrid();
-    }
+	constructor() {
+		this.#initalizeGrid();
+	}
 
-    #initalizeGrid() {
-        for (let i = 0; i < this.sideLength; i++) {
-            this.grid.push([]);
-            for(let j = 0; j < this.sideLength; j++) {
-                this.grid[i].push(0);
-                this.#updateGridCell(i, j, ...this.INITAL_CELL_STATE);
-            }
-        }
-    }
+	#initalizeGrid() {
+		for (let i = 0; i < this.sideLength; i++) {
+			this.grid.push([]);
+			for (let j = 0; j < this.sideLength; j++) {
+				this.grid[i].push(0);
+				this.#updateGridCell(i, j, ...this.INITAL_CELL_STATE);
+			}
+		}
+	}
 
-    #updateGridCell(row, column, occupyingShip, isAttacked) {
-        this.grid[row][column] = [occupyingShip, isAttacked];
-    }
+	#updateGridCell(row, column, occupyingShip, isAttacked) {
+		this.grid[row][column] = [occupyingShip, isAttacked];
+	}
 
-    placeShip(ship, row, column, direction) {
-        if(row < 0 || column < 0) {
-            throw new OutOfBoundsError(row, column, ship.length);
-        }
+	placeShip(ship, row, column, direction) {
+		if (
+			row < 0 ||
+			column < 0 ||
+			row > this.sideLength - 1 ||
+			column > this.sideLength - 1
+		) {
+			throw new OutOfBoundsError(row, column, ship.length);
+		}
 
-        switch(direction) {
-            case GameBoard.NORTH:
-                for(let i = 0; i < ship.length; i++) {
-                    this.#updateGridCell(row - i, column, ship, false);
-                }
-                break;
-            case GameBoard.EAST:
-                for(let i = 0; i < ship.length; i++) {
-                    this.#updateGridCell(row, column + i, ship, false);
-                }
-                break;
-            case GameBoard.SOUTH:
-                for(let i = 0; i < ship.length; i++) {
-                    this.#updateGridCell(row + i, column, ship, false);
-                }
-                break;
-            case GameBoard.WEST:
-                for(let i = 0; i < ship.length; i++) {
-                    this.#updateGridCell(row, column - i, ship, false);
-                }
-                break;
-        }
-    }
+		switch (direction) {
+			case GameBoard.NORTH:
+                if((row - ship.length) < 0) throw new OutOfBoundsError(row, column, ship.length);
+				for (let i = 0; i < ship.length; i++) {
+					this.#updateGridCell(row - i, column, ship, false);
+				}
+				break;
+			case GameBoard.EAST:
+                if((column + ship.length) > 10) throw new OutOfBoundsError(row, column, ship.length);
+				for (let i = 0; i < ship.length; i++) {
+					this.#updateGridCell(row, column + i, ship, false);
+				}
+				break;
+			case GameBoard.SOUTH:
+                if((row + ship.length) > 10) throw new OutOfBoundsError(row, column, ship.length);
+				for (let i = 0; i < ship.length; i++) {
+					this.#updateGridCell(row + i, column, ship, false);
+				}
+				break;
+			case GameBoard.WEST:
+                if((column + 1 - ship.length) < 0) throw new OutOfBoundsError(row, column, ship.length);
+				for (let i = 0; i < ship.length; i++) {
+					this.#updateGridCell(row, column - i, ship, false);
+				}
+				break;
+		}
+	}
 }
--- test/GameBoard.test.js
+++ test/GameBoard.test.js
@@ -5,108 +5,130 @@ import { OutOfBoundsError } from "../src/app/error/Error";
 let gameBoard;
 let expectedGrid;
 beforeEach(() => {
-    gameBoard = new GameBoard();
-    expectedGrid = [];
-    for (let i = 0; i < 10; i++) {
-        expectedGrid.push([]);
-        for (let j = 0; j < 10; j++) {
-            expectedGrid[i].push([null, false]);
-        }
-    }
+	gameBoard = new GameBoard();
+	expectedGrid = [];
+	for (let i = 0; i < 10; i++) {
+		expectedGrid.push([]);
+		for (let j = 0; j < 10; j++) {
+			expectedGrid[i].push([null, false]);
+		}
+	}
 });
 
 describe("GameBoard() constructor", () => {
-    test("Grid initalizes correctly", () => {
-        expect(gameBoard.grid).toEqual(expectedGrid);
-    });
+	test("Grid initalizes correctly", () => {
+		expect(gameBoard.grid).toEqual(expectedGrid);
+	});
 });
 
 describe("placeShip()", () => {
-    describe("places ship correctly in different points", () => {
-        test("place ship at [5, 3]", () => {
-            let newShip = new Ship(1);
-            expectedGrid[5][3] = [newShip, false];
+	describe("places ship correctly in different points", () => {
+		test("place ship at [5, 3]", () => {
+			let newShip = new Ship(1);
+			expectedGrid[5][3] = [newShip, false];
 
-            gameBoard.placeShip(newShip, 5, 3, GameBoard.EAST);
-            expect(gameBoard.grid).toEqual(expectedGrid);
-        });
+			gameBoard.placeShip(newShip, 5, 3, GameBoard.EAST);
+			expect(gameBoard.grid).toEqual(expectedGrid);
+		});
 
-        test("places ship on [0, 0]", () => {
-            let newShip = new Ship(1);
-            expectedGrid[0][0] = [newShip, false];
-            gameBoard.placeShip(newShip, 0, 0, GameBoard.EAST);
-            expect(gameBoard.grid).toEqual(expectedGrid);
-        });
+		test("places ship on [0, 0]", () => {
+			let newShip = new Ship(1);
+			expectedGrid[0][0] = [newShip, false];
+			gameBoard.placeShip(newShip, 0, 0, GameBoard.EAST);
+			expect(gameBoard.grid).toEqual(expectedGrid);
+		});
+	});
+
+	describe("places ship correctly with different lengths", () => {
+		test("place ship length 2", () => {
+			let newShip = new Ship(2);
+			expectedGrid[3][2] = [newShip, false];
+			expectedGrid[3][3] = [newShip, false];
+
+			gameBoard.placeShip(newShip, 3, 2, GameBoard.EAST);
+			expect(gameBoard.grid).toEqual(expectedGrid);
+		});
+
+		test("place ship length 4", () => {
+			let newShip = new Ship(4);
+			expectedGrid[3][2] = [newShip, false];
+			expectedGrid[3][3] = [newShip, false];
+			expectedGrid[3][4] = [newShip, false];
+			expectedGrid[3][5] = [newShip, false];
+
+			gameBoard.placeShip(newShip, 3, 2, GameBoard.EAST);
+			expect(gameBoard.grid).toEqual(expectedGrid);
+		});
+	});
+
+	describe("places ship correctly in different directions", () => {
+		test("place ship facing north", () => {
+			let newShip = new Ship(3);
+			expectedGrid[3][2] = [newShip, false];
+			expectedGrid[2][2] = [newShip, false];
+			expectedGrid[1][2] = [newShip, false];
+
+			gameBoard.placeShip(newShip, 3, 2, GameBoard.NORTH);
+			expect(gameBoard.grid).toEqual(expectedGrid);
+		});
+
+		test("place ship facing east", () => {
+			let newShip = new Ship(3);
+			expectedGrid[3][2] = [newShip, false];
+			expectedGrid[3][3] = [newShip, false];
+			expectedGrid[3][4] = [newShip, false];
+
+			gameBoard.placeShip(newShip, 3, 2, GameBoard.EAST);
+			expect(gameBoard.grid).toEqual(expectedGrid);
+		});
+
+		test("place ship facing south", () => {
+			let newShip = new Ship(3);
+			expectedGrid[3][2] = [newShip, false];
+			expectedGrid[4][2] = [newShip, false];
+			expectedGrid[5][2] = [newShip, false];
+
+			gameBoard.placeShip(newShip, 3, 2, GameBoard.SOUTH);
+			expect(gameBoard.grid).toEqual(expectedGrid);
+		});
+
+		test("place ship facing west", () => {
+			let newShip = new Ship(3);
+			expectedGrid[3][2] = [newShip, false];
+			expectedGrid[3][1] = [newShip, false];
+			expectedGrid[3][0] = [newShip, false];
+
+			gameBoard.placeShip(newShip, 3, 2, GameBoard.WEST);
+			expect(gameBoard.grid).toEqual(expectedGrid);
+		});
+	});
+
+	describe("placing ship out of bounds throws OutOfBoundsError", () => {
+		test("placing ship at [-1, -1] bounds throws OutOfBoundsError", () => {
+			expect(() => {
+				let newShip = new Ship(1);
+				gameBoard.placeShip(newShip, -1, -1, GameBoard.NORTH);
+			}).toThrow(OutOfBoundsError);
+		});
+
+		test("placing ship at [11, 11] bounds throws OutOfBoundsError", () => {
+			expect(() => {
+				let newShip = new Ship(1);
+				gameBoard.placeShip(newShip, 11, 11, GameBoard.NORTH);
+			}).toThrow(OutOfBoundsError);
+		});
+
+		test("place ship in bounds but runs out of bounds throws OutOfBoundsError", () => {
+			
+		});
+	});
+
+    describe("place ship in bounds but runs out of bounds throws OutOfBoundsError", () => {
+        test("ship runs out of bounds south", () => {
+            expect(() => {
+				let newShip = new Ship(5);
+				gameBoard.placeShip(newShip, 10, 5, GameBoard.SOUTH);
+			});
+        })
     });
-
-    describe("places ship correctly with different lengths", () => {
-        test("place ship length 2", () => {
-            let newShip = new Ship(2);
-            expectedGrid[3][2] = [newShip, false];
-            expectedGrid[3][3] = [newShip, false];
-
-            gameBoard.placeShip(newShip, 3, 2, GameBoard.EAST);
-            expect(gameBoard.grid).toEqual(expectedGrid);
-        });
-
-        test("place ship length 4", () => {
-            let newShip = new Ship(4);
-            expectedGrid[3][2] = [newShip, false];
-            expectedGrid[3][3] = [newShip, false];
-            expectedGrid[3][4] = [newShip, false];
-            expectedGrid[3][5] = [newShip, false];
-
-            gameBoard.placeShip(newShip, 3, 2, GameBoard.EAST);
-            expect(gameBoard.grid).toEqual(expectedGrid);
-        });
-    });
-
-    describe("places ship correctly in different directions", () => {
-        test("place ship facing north", () => {
-            let newShip = new Ship(3);
-            expectedGrid[3][2] = [newShip, false];
-            expectedGrid[2][2] = [newShip, false];
-            expectedGrid[1][2] = [newShip, false];
-
-            gameBoard.placeShip(newShip, 3, 2, GameBoard.NORTH);
-            expect(gameBoard.grid).toEqual(expectedGrid);
-        });
-
-        test("place ship facing east", () => {
-            let newShip = new Ship(3);
-            expectedGrid[3][2] = [newShip, false];
-            expectedGrid[3][3] = [newShip, false];
-            expectedGrid[3][4] = [newShip, false];
-
-            gameBoard.placeShip(newShip, 3, 2, GameBoard.EAST);
-            expect(gameBoard.grid).toEqual(expectedGrid);
-        });
-
-        test("place ship facing south", () => {
-            let newShip = new Ship(3);
-            expectedGrid[3][2] = [newShip, false];
-            expectedGrid[4][2] = [newShip, false];
-            expectedGrid[5][2] = [newShip, false];
-
-            gameBoard.placeShip(newShip, 3, 2, GameBoard.SOUTH);
-            expect(gameBoard.grid).toEqual(expectedGrid);
-        });
-
-        test("place ship facing west", () => {
-            let newShip = new Ship(3);
-            expectedGrid[3][2] = [newShip, false];
-            expectedGrid[3][1] = [newShip, false];
-            expectedGrid[3][0] = [newShip, false];
-
-            gameBoard.placeShip(newShip, 3, 2, GameBoard.WEST);
-            expect(gameBoard.grid).toEqual(expectedGrid);
-        });
-    });
-
-    test("placing ship out of bounds throws OutOfBoundsError", () => {
-        expect(() => {
-            let newShip = new Ship(1);
-            gameBoard.placeShip(newShip, -1, -1, GameBoard.NORTH);
-        }).toThrow(OutOfBoundsError);
-    })
 });
