697663d43e367c36f39d206976b52df98b2fccad
Proxtron <bratan_pavel@yahoo.com>
2025-08-07 19:52:22 -0700

    Import typed

---
+++ src/app/controller/NameCollectionController.js
@@ -0,0 +1,11 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#typed", {
+    strings: [
+        "Hello I am testing typed",
+    ]
+});
+
+typed.start();
+typed.toggle();
+typed.toggle();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./css/reset.css";
 import "./css/style.css";
+import "./app/controller/NameCollectionController";
 import "./app/controller/DOMController";
 import "./app/controller/EndGameModalController";
