94eb0692739b5fda6adbb4dcfa7906bd904cdecd
Proxtron <51761303+Proxtron@users.noreply.github.com>
2025-08-19 15:21:58 -0700

    Place ship on gameboard on drop event

--- src/app/controller/ShipPlacementController.js
+++ src/app/controller/ShipPlacementController.js
@@ -11,54 +11,71 @@ const shipPlacementRightCol = document.getElementById("sp-right-col");
 let placeShipRandomlyBtn;
 
 export function showShipPlacementScreen(name) {
-    placeShipRandomlyBtn = PlaceRandomlyButtonView();
-    shipPlacementLeftCol.append(PlacementInstructionsView(name), placeShipRandomlyBtn, PlaceableShipView(5));
-    renderPlacementGrid(game.player1.gameBoard);
-   
-    new Typed("#placement-instructions", {
-        strings: [`It's time to place your ships, ${name}! Drag and drop and press "R" to rotate.`],
-        showCursor: false
-    });
+	placeShipRandomlyBtn = PlaceRandomlyButtonView();
+	shipPlacementLeftCol.append(
+		PlacementInstructionsView(name),
+		placeShipRandomlyBtn,
+		PlaceableShipView(5),
+		PlaceableShipView(4),
+		PlaceableShipView(3),
+		PlaceableShipView(3),
+		PlaceableShipView(2),
+	);
+	renderPlacementGrid(game.player1.gameBoard);
 
-    addEventListeners();
+	new Typed("#placement-instructions", {
+		strings: [
+			`It's time to place your ships, ${name}! Drag and drop and press "R" to rotate.`,
+		],
+		showCursor: false,
+	});
+
+	addEventListeners();
 }
 
 function renderPlacementGrid(gameBoard) {
-    shipPlacementRightCol.innerHTML = "";
-    shipPlacementRightCol.append(PlacingGameBoardView(gameBoard));
+	shipPlacementRightCol.innerHTML = "";
+	shipPlacementRightCol.append(PlacingGameBoardView(gameBoard));
+
+    shipPlacementRightCol.querySelectorAll(".drop-target").forEach((cell) => {
+		cell.addEventListener("dragover", dragOverHandler);
+	});
+
+	shipPlacementRightCol.querySelectorAll(".drop-target").forEach((cell) => {
+		cell.addEventListener("drop", dropHandler);
+	});
 }
 
 function addEventListeners() {
-    placeShipRandomlyBtn.addEventListener("click", () => {
-        game.clearHumanShips();
-        game.placeHumanShipsRandomly();
-        renderPlacementGrid(game.player1.gameBoard);
-    });
-    
-    shipPlacementLeftCol.querySelectorAll(".draggable").forEach((ship) => {
-        ship.addEventListener("dragstart", dragStartHandler);
-    });
+	placeShipRandomlyBtn.addEventListener("click", () => {
+		game.clearHumanShips();
+		game.placeHumanShipsRandomly();
+		renderPlacementGrid(game.player1.gameBoard);
+	});
 
-    shipPlacementRightCol.querySelectorAll(".drop-target").forEach((cell) => {
-        cell.addEventListener("dragover", dragOverHandler);
-    });
-
-    shipPlacementRightCol.querySelectorAll(".drop-target").forEach((cell) => {
-        cell.addEventListener("drop", dropHandler);
-    });
+	shipPlacementLeftCol.querySelectorAll(".draggable").forEach((ship) => {
+		ship.addEventListener("dragstart", dragStartHandler);
+	});
 }
 
 function dragStartHandler(event) {
-    console.log(event);
+	const shipLength = event.srcElement.dataset.length;
+	event.dataTransfer.setData("length", shipLength);
 }
 
 function dragOverHandler(event) {
-    event.preventDefault();
-    console.log(event);
+	event.preventDefault();
 }
 
 function dropHandler(event) {
-    event.preventDefault();
-    game.placeSingleHumanShip(5, 0, 0 , GameBoard.SOUTH);
-    renderPlacementGrid(game.player1.gameBoard);
+	event.preventDefault();
+
+	const dropTarget = event.target;
+	const placeX = parseInt(dropTarget.dataset.col);
+	const placeY = parseInt(dropTarget.dataset.row);
+	const length = parseInt(event.dataTransfer.getData("length"));
+
+	game.placeSingleHumanShip(length, placeY, placeX, GameBoard.EAST);
+
+	renderPlacementGrid(game.player1.gameBoard);
 }
--- src/app/view/PlaceableShipView.js
+++ src/app/view/PlaceableShipView.js
@@ -2,6 +2,7 @@ export default function PlaceableShipView(length) {
     const div = document.createElement("div");
     div.classList.add("draggable");
     div.draggable = "true";
+    div.dataset.length = length;
     for(let i = 0; i < length; i++) {
         div.innerHTML += `<div class="grid-cell placeable-grid-cell"></div>`;
     }
